d260d69112aabdbf686b7696bfd37db79366cd25
Komal_Solanki <69202411+komal2605@users.noreply.github.com>
2024-03-06 15:54:16 +0530

    logo fixed, scroll hide

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,25 +7,25 @@ import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import gsap from "gsap";
 
 const Navbar = () => {
-	useEffect(() => {
-		gsap.registerPlugin(ScrollTrigger);
-		const tl = gsap.timeline({
-			scrollTrigger: {
-				trigger: "#animation",
-				start: "200% top",
-				end: "=+1000",
-				scrub: true,
-				// markers: true,
-			},
-		});
-		tl.to(".navbar", {
-			opacity: 0,
-			y: -50,
-		});
-	}, []);
+	// useEffect(() => {
+	// 	gsap.registerPlugin(ScrollTrigger);
+	// 	const tl = gsap.timeline({
+	// 		scrollTrigger: {
+	// 			trigger: "#animation",
+	// 			start: "200% top",
+	// 			end: "=+1000",
+	// 			scrub: true,
+	// 			// markers: true,
+	// 		},
+	// 	});
+	// 	tl.to(".navbar", {
+	// 		opacity: 0,
+	// 		y: -50,
+	// 	});
+	// }, []);
 	return (
 		<div className="navbar">
-			<Image src={logo} width={70} height={0} alt="logo" />
+			<Image src={logo} width={68} height={0} alt="logo" />
 		</div>
 	);
 };
