2fa8add63bd18a7b6f16d4a11f19d69767c871ba
Nakul Gupta <nakulgupta1042@gmail.com>
2024-03-06 11:56:37 +0530

    Code formatted

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,9 @@ import FixedSection from "@/section/FixedWrapper/FixedSection";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <FixedSection />
-    </main>
-  );
+	return (
+		<main className={styles.main}>
+			<FixedSection />
+		</main>
+	);
 }
--- src/section/demo/AnimateLoader.jsx
+++ src/section/demo/AnimateLoader.jsx
@@ -6,86 +6,86 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import animation from "@/assets/animation.json";
 import Lenis from "@studio-freight/lenis";
 export default function AnimatedLoader() {
-  const [instance, setInstance] = useState();
-  const playerRef = useRef();
-  const animationRef = useRef();
-  gsap.registerPlugin(ScrollTrigger);
-  const lenis = new Lenis();
+	const [instance, setInstance] = useState();
+	const playerRef = useRef();
+	const animationRef = useRef();
+	gsap.registerPlugin(ScrollTrigger);
+	const lenis = new Lenis();
 
-  lenis.on("scroll", (e) => {
-    // console.log(e);
-  });
+	lenis.on("scroll", (e) => {
+		// console.log(e);
+	});
 
-  function raf(time) {
-    lenis.raf(time);
-    requestAnimationFrame(raf);
-  }
+	function raf(time) {
+		lenis.raf(time);
+		requestAnimationFrame(raf);
+	}
 
-  requestAnimationFrame(raf);
-  useEffect(() => {
-    ScrollTrigger.defaults({
-      // markers: true,
-    });
-    gsap.registerPlugin(ScrollTrigger);
-  }, []);
+	requestAnimationFrame(raf);
+	useEffect(() => {
+		ScrollTrigger.defaults({
+			// markers: true,
+		});
+		gsap.registerPlugin(ScrollTrigger);
+	}, []);
 
-  useEffect(() => {
-    playerRef.current.setSeeker("20%");
-  }, []);
+	useEffect(() => {
+		playerRef.current.setSeeker("20%");
+	}, []);
 
-  useEffect(() => {
-    if (!instance) return;
+	useEffect(() => {
+		if (!instance) return;
 
-    const onUpdate = (self) => {
-      const progress = self.progress;
-      console.log(progress, "progress");
-      const currentFrame = Math.round(instance.totalFrames * progress);
-      instance.goToAndStop(currentFrame, true);
+		const onUpdate = (self) => {
+			const progress = self.progress;
+			console.log(progress, "progress");
+			const currentFrame = Math.round(instance.totalFrames * progress);
+			instance.goToAndStop(currentFrame, true);
 
-      if (progress === 1) {
-        playerRef.current.setSeeker("100%");
-      }
-      const animationHeight = instance.totalFrames * 5;
-      console.log(animationHeight, "animationHeight");
-      document.body.style.height = `${animationHeight - 2200}px`;
-    };
+			if (progress === 1) {
+				playerRef.current.setSeeker("100%");
+			}
+			const animationHeight = instance.totalFrames * 5;
+			console.log(animationHeight, "animationHeight");
+			document.body.style.height = `${animationHeight - 2200}px`;
+		};
 
-    ScrollTrigger.create({
-      trigger: animationRef.current,
-      // pin: true,
-      start: "-50% 50%",
-      end: () => `+=${instance.totalFrames * 5}`,
-      scrub: true,
-      onUpdate: onUpdate,
-    });
+		ScrollTrigger.create({
+			trigger: animationRef.current,
+			// pin: true,
+			start: "-50% 50%",
+			end: () => `+=${instance.totalFrames * 5}`,
+			scrub: true,
+			onUpdate: onUpdate,
+		});
 
-    return () => {
-      ScrollTrigger.getAll().forEach((trigger) => trigger.revert());
-    };
-  }, [instance]);
+		return () => {
+			ScrollTrigger.getAll().forEach((trigger) => trigger.revert());
+		};
+	}, [instance]);
 
-  return (
-    <div
-      id="animation"
-      ref={animationRef}
-      style={{
-        // border: "1px solid red",
-        width: "50%",
-        height: "max-content",
-      }}
-    >
-      <Player
-        lottieRef={(ins) => {
-          setInstance(ins);
-        }}
-        autoplay={false}
-        setSeeker={"10%"}
-        loop={false}
-        controls={true}
-        src={animation}
-        style={{ width: "100%", height: "100%" }}
-        ref={playerRef}
-      />
-    </div>
-  );
+	return (
+		<div
+			id="animation"
+			ref={animationRef}
+			style={{
+				// border: "1px solid red",
+				width: "50%",
+				height: "max-content",
+			}}
+		>
+			<Player
+				lottieRef={(ins) => {
+					setInstance(ins);
+				}}
+				autoplay={false}
+				setSeeker={"10%"}
+				loop={false}
+				controls={true}
+				src={animation}
+				style={{ width: "100%", height: "100%" }}
+				ref={playerRef}
+			/>
+		</div>
+	);
 }
