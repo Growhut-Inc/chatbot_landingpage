abdad34a2ce3a0e1087d52a2bace3aaf513aa98d
Nakul Gupta <nakulgupta1042@gmail.com>
2024-03-18 13:36:08 +0530

    preload-animations

--- src/components/LottieScrollTrigger/LottieScrollTrigger.jsx
+++ src/components/LottieScrollTrigger/LottieScrollTrigger.jsx
@@ -7,73 +7,135 @@ import { Observer } from "gsap/Observer";
 gsap.registerPlugin(ScrollTrigger, Observer);
 
 function LottieScrollTrigger() {
-	const animationContainer = useRef(null);
-	const animationInstance = useRef(null);
+	const preloadedAnimations = useRef([]);
+	const maxLength = 8;
+	const animationContainer = useRef(
+		[...Array(maxLength)].map(() => React.createRef())
+	);
 	const currentIndexRef = useRef(0);
 	const isAnimating = useRef(false);
-	const maxLength = 8;
 
 	const loadAnimation = (index, direction) => {
-		if (animationInstance.current) {
-			animationInstance.current.destroy();
+		const newAnimation = preloadedAnimations.current[index];
+		if (!newAnimation) return;
+		for (var i = 0; i < maxLength; i++) {
+			animationContainer.current[i].current.style.display = "none";
 		}
-		animationInstance.current = lottie.loadAnimation({
-			container: animationContainer.current,
-			renderer: "svg",
-			loop: false,
-			autoplay: false,
-			path: `/animations/lottie${index + (direction === 1 ? 1 : 2)}.json`,
-		});
-
-		animationInstance.current.addEventListener("DOMLoaded", () => {
-			if (direction === -1) {
-				animationInstance.current.goToAndStop(
-					animationInstance.current.totalFrames - 1,
-					true
-				);
-				animationInstance.current.setDirection(-1);
-			} else {
-				animationInstance.current.setDirection(1);
-			}
-			animationInstance.current.play();
-			isAnimating.current = true;
-			animationInstance.current.addEventListener("complete", () => {
-				isAnimating.current = false;
-			});
-		});
+		animationContainer.current[index].current.style.display = "block";
+		if (direction === -1) {
+			newAnimation.goToAndStop(newAnimation.totalFrames - 1, true);
+			newAnimation.setDirection(-1);
+		} else {
+			newAnimation.goToAndStop(0, true);
+			newAnimation.setDirection(1);
+		}
+		newAnimation.play();
+		isAnimating.current = true;
 	};
 
 	const handleScroll = (direction) => {
 		if (isAnimating.current) return;
 		let newIndex = currentIndexRef.current + direction;
+		console.log("newIndex", newIndex);
 		if (newIndex < -1 || newIndex >= maxLength) return;
 		isAnimating.current = true;
 		currentIndexRef.current = newIndex;
 		loadAnimation(currentIndexRef.current, direction);
 	};
 
+	const debounce = (func, wait) => {
+		let timeout;
+
+		return function executedFunction(...args) {
+			const later = () => {
+				clearTimeout(timeout);
+				func(...args);
+			};
+
+			clearTimeout(timeout);
+			timeout = setTimeout(later, wait);
+		};
+	};
+
+	const debouncedHandleScroll = debounce(handleScroll, 200);
+
 	useEffect(() => {
 		Observer.create({
 			type: "wheel,touch,pointer",
 			wheelSpeed: -1,
-			onDown: () => handleScroll(-1),
-			onUp: () => handleScroll(1),
+			onDown: () => debouncedHandleScroll(-1),
+			onUp: () => debouncedHandleScroll(1),
 			tolerance: 10,
 		});
 
-		loadAnimation(0, 1);
+		const animationPromises = animationContainer.current.map(
+			(ref, index) =>
+				new Promise((resolve, reject) => {
+					if (ref.current) {
+						const anim = lottie.loadAnimation({
+							container: ref.current,
+							renderer: "svg",
+							loop: false,
+							autoplay: false,
+							path: `/animations/lottie${index + 1}.json`,
+						});
+						anim.addEventListener("DOMLoaded", () => {
+							resolve(anim);
+						});
+						anim.addEventListener("complete", () => {
+							isAnimating.current = false;
+						});
+						preloadedAnimations.current.push(anim);
+					} else {
+						reject(
+							new Error(
+								`Container for animation ${
+									index + 1
+								} is not available`
+							)
+						);
+					}
+				})
+		);
+
+		Promise.all(animationPromises).then(() => {
+			loadAnimation(0, 1);
+		});
 
 		return () => {
-			animationInstance.current?.destroy();
+			preloadedAnimations.current.forEach((anim) => anim.destroy());
 			ScrollTrigger.getAll().forEach((st) => st.kill());
 		};
 	}, []);
 
 	return (
 		<div
-			ref={animationContainer}
-			style={{ width: "100%", height: "40vh" }}
-		/>
+			style={{
+				overflow: "hidden",
+				height: "100vh",
+				position: "relative",
+			}}
+		>
+			{Array.from({ length: maxLength }).map((_, i) => (
+				<div
+					style={{
+						width: "100%",
+						height: "100vh",
+						position: "absolute",
+					}}
+					key={i}
+				>
+					<div
+						ref={animationContainer.current[i]}
+						style={{
+							width: "100%",
+							height: "40vh",
+							display: "none",
+						}}
+					/>
+				</div>
+			))}
+		</div>
 	);
 }
 
